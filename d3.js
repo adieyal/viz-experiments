ddb2e55b13455c8ffe1b47d022d89f820af3227f
Adi Eyal <adi@openup.org.za>
2020-10-04 20:01:28 +0200

    Applied linter to code

--- d3.js
+++ d3.js
@@ -1,25 +1,40 @@
-import {select, selectAll, create} from 'd3-selection';
-import {csv, json} from 'd3-fetch';
-import {randomInt} from 'd3-random';
-import {max, min, maxIndex, minIndex, extent} from 'd3-array';
-import {scaleLinear, scaleSqrt, scaleOrdinal, scaleSequential} from 'd3-scale';
+import { select, selectAll, create } from 'd3-selection'
+import { csv, json } from 'd3-fetch'
+import { randomInt } from 'd3-random'
+import { max, min, maxIndex, minIndex, extent } from 'd3-array'
+import { scaleLinear, scaleSqrt, scaleOrdinal, scaleSequential } from 'd3-scale'
 import {
-    forceSimulation, forceCenter, forceManyBody,
-    forceCollide, forceX, forceY
-} from 'd3-force';
-import {transition} from 'd3-transition';
-import tip from 'd3-tip';
-import {format} from 'd3-format';
+  forceSimulation, forceCenter, forceManyBody,
+  forceCollide, forceX, forceY
+} from 'd3-force'
+import { transition } from 'd3-transition'
+import tip from 'd3-tip'
+import { format } from 'd3-format'
 
 const d3 = {
-    select, selectAll, create,
-    csv, json,
-    randomInt,
-    scaleLinear, scaleSqrt, scaleOrdinal, scaleSequential,
-    max, min, maxIndex, minIndex, extent,
-    forceSimulation, forceCenter, forceManyBody, forceCollide, forceX, forceY,
-    tip,
-    format,
+  select,
+  selectAll,
+  create,
+  csv,
+  json,
+  randomInt,
+  scaleLinear,
+  scaleSqrt,
+  scaleOrdinal,
+  scaleSequential,
+  max,
+  min,
+  maxIndex,
+  minIndex,
+  extent,
+  forceSimulation,
+  forceCenter,
+  forceManyBody,
+  forceCollide,
+  forceX,
+  forceY,
+  tip,
+  format
 }
 
-export default d3;
+export default d3
